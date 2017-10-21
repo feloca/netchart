e081a823f25be76186dffc72cd6753d6f9291744
feloca <aflc82@gmail.com>
2017-10-21 04:22:24 +0200

    Añadido el layaout del gráfico, ejes y titulo.

--- NetChart.Web/Scripts/NetChart.js
+++ NetChart.Web/Scripts/NetChart.js
@@ -9,7 +9,7 @@
 !function (window) {
     console.log('cargo netchart');
     nc = {
-        version:1.0
+        version: 1.0
     };
 
     //Espacio de nombre de svg
@@ -56,10 +56,10 @@
 
         switch (nc_types[dataObj.ChartType]) {
             case 'Debug':
-                nc_drawDebug(dataObj);
+                nc_drawChartDebug(dataObj);
                 break;
             case 'Bar':
-                nc_drawBar(dataObj);
+                nc_drawChartBar(dataObj);
                 break;
             case 'Line':
                 break;
@@ -78,45 +78,115 @@
         }
     }
 
-    function nc_drawDebug(data) {
+    function nc_drawChartDebug(data) {
 
     }
 
-    function nc_drawBar(data) {
+    function nc_drawChartBar(data) {
         //TODO: sacar la creación del svg a una funcion
         //var svg = nc_document.createElementNS(nc_svgns, 'svg');
         //nc_appendStyleAttribute(svg, 'width', '100%');
         //nc_appendStyleAttribute(svg, 'height', '100%');
-        var svg = nc_getRootSVG();
+        var svgRoot = nc_getSVGRoot();
 
-        let svgHeight = nc_selection.clientHeight;
-        let svgWidth = nc_selection.clientWidth;
-        let dataCount = data.VariableData.length; //OJO: si metemos titulo o ejes, habria que reservar espacio
+        let svgRootWidth = nc_selection.clientWidth;
+        let svgRootHeight = nc_selection.clientHeight;
+
+        //OJO: si metemos titulo o ejes, habria que reservar espacio
         //todo: meter porcentajes para que no toquen los bordes del contenedor
+        //todo: el alto y el ancho del area disponible, aplicar una funcion para poner el titulo y los ejes, 
+        //deberia de quedar un cuadro mas pequeño para el gráfico
+        //https://stackoverflow.com/questions/479591/svg-positioning
+        //meter el grafico en un "g" y transladarlo, o crear SVG nuevo para el gráfico
+        //el problema de g es que no recorta lo que queda afuera de sus dimensiones
+
+        //aqui me he quedado, creo que esta funcion podria poner el titulo y los ejes
+        //function nc_createSVGChart(svgRoot, svgRootWidth, svgRootHeight, y data)
+        let svgChart = nc_createSVGChartLayout(svgRoot, svgRootWidth, svgRootHeight, data);
+        var chartWidth = svgChart.width.baseVal.value
+        var chartHeight = svgChart.height.baseVal.value;
+        //alert(chartHeight);
+
+        //let svgChart = nc_createSVG(svgRoot, chartX, chartY, chartWidth, chartHeight);
+        let maxRange = nc_maxValue(data.VariableData);
+        let minRange = nc_minValue(data.VariableData);
+        if (minRange > 0) {
+            minRange = 0;
+        }//todo: si el rango inferior es negativo restarle un 5% (max-min*0.05) para que aparezca el valor minimo
+
+        let scaleY = nc_createScaleLinear(0, chartHeight, minRange, maxRange)
+        //let columnCount = data.VariableData.length;
+        let columnWidth = chartWidth / data.VariableData.length;
 
         //calcular la escala
         for (let i = 0; i < data.DimensionData.length; ++i) {
-            let dDimension = data.DimensionData[i];
-            let dVariable = data.VariableData[i];
-            let laY = svgHeight - dDimension;
-            nc_createRect(svg, i * 10, laY, 10, dDimension, 'teal');
+            //let dDimension = data.DimensionData[i];
+            //todo: falta meter la escala a x
+            let dVariable = scaleY.getDomainValue(data.VariableData[i]);
+            nc_createRect(svgChart, i * columnWidth, chartHeight - dVariable, columnWidth, dVariable, 'teal');
         }
 
+        //Añado al final el nodo para evitar que el gráfico aparezca a golpes
         nc_selection.innerHTML = '';
-        nc_appendChild(nc_selection, svg);
+        nc_selection.appendChild(svgRoot);
+        //nc_appendChild(nc_selection, svgRoot);
     }
 
-    //function nc_drawLine() { }
-    //function nc_drawScatter() { }
-    //function nc_drawBubble() { }
-    //function nc_drawTemperature() { }
-    //function nc_drawPie() { }
-    //function nc_drawRadar() { }
+
+    //Crea el svg donde se dibuja el gráfico, ademas añade los ejes, leyendas y titulo
+    function nc_createSVGChartLayout(parentSVG, width, height, data) {
+        //todo: cuando defina los parametros de configuración (titulo, leyenda,etc) meterlos aqui
+        let chartX = 30,
+            chartY = 30,
+            chartWidth = width - (chartX * 2),
+            chartHeight = height - (chartY * 2);
+        let svgChart = nc_createSVG(parentSVG, chartX, chartY, chartWidth, chartHeight);
+
+        //creamos el eje y
+        let maxRange = nc_maxValue(data.VariableData);
+        let minRange = nc_minValue(data.VariableData);
+        if (minRange > 0) {
+            minRange = 0;
+        }//todo: revisar si gestiono negativos
+
+        //434 y max, y coge 494
+        let scaleY = nc_createScaleLinear(0, chartHeight, minRange, maxRange)
+        nc_createLine(parentSVG, chartX, chartY, chartX, chartY + chartHeight);
+        for (let i = 0; i < data.VariableData.length; ++i) {
+            nc_createText(parentSVG, chartX - 20,
+                chartHeight - scaleY.getDomainValue(data.VariableData[i]) + chartY,
+                data.VariableData[i]);
+        }
+
+        //creamos el eje x
+        nc_createLine(parentSVG, chartX, chartY + chartHeight, chartX + chartWidth, chartY + chartHeight);
+        let columnWidth =chartWidth/data.DimensionData.length;
+        let columnCenter = columnWidth/2;
+        for (let i = 0; i < data.DimensionData.length; ++i) {
+            nc_createText(parentSVG, (i*columnWidth) + columnCenter, chartY + chartHeight + 15, data.DimensionData[i]);
+        }
+
+        //creo el titulo
+        let title = nc_createText(parentSVG, '50%', 20, 'EL TITULO');
+        //alignment-baseline="middle" text-anchor="middle"
+        //nc_appendAttribute(title, 'alignment-baseline', 'middle');
+        nc_appendAttribute(title, 'text-anchor', 'middle');
+        nc_appendAttribute(title, 'font-weight', 'bold');
+
+        return svgChart;
+    }
+
+    //function nc_drawChartLine() { }
+    //function nc_drawChartScatter() { }
+    //function nc_drawChartBubble() { }
+    //function nc_drawChartTemperature() { }
+    //function nc_drawChartPie() { }
+    //function nc_drawChartRadar() { }
 
     //Añade un nodo hijo a un nodo
-    function nc_appendChild(parentNode, childNode) {
-        parentNode.appendChild(childNode);
-    }
+    //function nc_appendChild(parentNode, childNode) {
+    //    parentNode.appendChild(childNode);
+    //}
 
     //Añade un atributo a un nodo
     function nc_appendAttribute(node, attrName, attrValue) {
@@ -131,7 +201,49 @@
         node.style[attrName] = attrValue;
     }
 
+    //Crea un nodo SVG
+    function nc_createSVG(parentNode, x, y, width, height) {
+        let svg = nc_document.createElementNS(nc_svgns, 'svg');
+        nc_appendAttribute(svg, 'x', x);
+        nc_appendAttribute(svg, 'y', y);
+        nc_appendAttribute(svg, 'width', width);
+        nc_appendAttribute(svg, 'height', height);
+        //nc_appendStyleAttribute(svg, 'width', width);
+        //nc_appendStyleAttribute(svg, 'height', height);
+        parentNode.appendChild(svg);
+        return svg;
+    }
+
     //automatizar la creacion de las formas, rectangulos, burbujas, sectores, triangulos y lineas
+
+    //Crea un texto svg
+    function nc_createText(parentNode, x, y, text) {
+        let svgText = nc_document.createElementNS(nc_svgns, 'text');
+        let textNode = nc_document.createTextNode(text);
+        svgText.appendChild(textNode);
+        nc_appendAttribute(svgText, 'x', x);
+        nc_appendAttribute(svgText, 'y', y);
+        parentNode.appendChild(svgText);
+        return svgText;
+    }
+
+    //Crea una linea svg
+    function nc_createLine(parentNode, x1, y1, x2, y2) {
+        let line = nc_document.createElementNS(nc_svgns, 'line');
+        nc_appendAttribute(line, 'x1', x1);
+        nc_appendAttribute(line, 'y1', y1);
+        nc_appendAttribute(line, 'x2', x2);
+        nc_appendAttribute(line, 'y2', y2);
+
+        //tb va con estilos
+        //nc_appendStyleAttribute(line, 'stroke', 'black');
+        //nc_appendStyleAttribute(line, 'stroke-width', 2);
+        nc_appendAttribute(line, 'stroke', 'black');
+        nc_appendAttribute(line, 'stroke-width', 2);
+        parentNode.appendChild(line);
+    }
+
+    //Crea un rectangulo svg
     function nc_createRect(parentNode, x, y, width, height, color) {
         let rect = nc_document.createElementNS(nc_svgns, 'rect');
         nc_appendAttribute(rect, 'x', x);
@@ -143,13 +255,14 @@
     }
 
     //Crea un svg con los estilos apropiados para ser el contenedor base del gráfico
-    function nc_getRootSVG() {
+    function nc_getSVGRoot() {
         let svg = nc_document.createElementNS(nc_svgns, 'svg');
         nc_appendStyleAttribute(svg, 'width', '100%');
         nc_appendStyleAttribute(svg, 'height', '100%');
-
         return svg;
     }
+
+    //VA AQUI
 
     //FUNCIONES AUXILIARES
     //max
@@ -185,7 +298,7 @@
     }
 
     //Crea una escala lineal
-    function nc_createScaleLinear(domainMin, domainMax, rangeMin, rangeMax){
+    function nc_createScaleLinear(domainMin, domainMax, rangeMin, rangeMax) {
         let scale = {
             domainMin: domainMin,
             domainMax: domainMax,
@@ -196,7 +309,7 @@
                 let outputGap = domainMax - domainMin;
                 let inputGap = rangeMax - rangeMin;
                 let inputValue = rangeValue - rangeMin; //celda d3
-                
+
                 //calculo % sobre rango
                 let rangePercentage = (inputValue * 100) / inputGap;
 
